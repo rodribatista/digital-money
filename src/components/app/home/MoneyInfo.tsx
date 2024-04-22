50c98d29129be20a331cb4bf230e5a39692015a2
Rodrigo <rodrigo@192.168.1.20>
2024-04-22 13:49:33 -0300

    [fix] linked cvu button to profile page

--- src/components/app/home/MoneyInfo.tsx
+++ src/components/app/home/MoneyInfo.tsx
@@ -5,7 +5,7 @@ export const MoneyInfo = () => {
     <section className={"w-full p-5 flex flex-col gap-5 rounded-md bg-black shadow-md md:p-10 xl:p-15"}>
       <div className={"flex flex-row gap-5 self-end"}>
         <Link href={"/app/cards"} className={"border-b text-sm"}>Ver tarjetas</Link>
-        <Link href={"#"} className={"border-b text-sm"}>Ver CVU</Link>
+        <Link href={"/app/profile"} className={"border-b text-sm"}>Ver CVU</Link>
       </div>
       <div className={"flex flex-col gap-5"}>
         <h2 className={"text-xl"}>Dinero disponible</h2>
