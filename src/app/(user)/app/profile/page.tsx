e59cbb2212d3d035d2c1fd53085e440cea4ee0ca
Rodrigo <rodrigo@192.168.1.20>
2024-04-22 17:45:06 -0300

    [fix] change profile page title

--- src/app/(user)/app/profile/page.tsx
+++ src/app/(user)/app/profile/page.tsx
@@ -4,7 +4,7 @@ import {AccountsInfo} from "@/components/app/profile/AccountsInfo";
 const ProfilePage = () => {
   return (
     <>
-      <h1 className={"self-start text-black font-semibold"}>Mi perfil</h1>
+      <h1 className={"self-start text-black font-semibold"}>Tu perfil</h1>
       <AppLinks linkTo={"/app/cards"}>Gestionar los medios de pago</AppLinks>
       <AccountsInfo/>
     </>
