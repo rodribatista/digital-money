dbebbbe0b363761ecd724ece370a81432d23407e
Rodrigo <rodrigo@192.168.1.20>
2024-04-29 18:09:57 -0300

    [dev] add logo imgs for services

--- src/components/dash/payment/ServiceItem.tsx
+++ src/components/dash/payment/ServiceItem.tsx
@@ -1,12 +1,16 @@
+import Image from "next/image";
+
 import {ServiceSelect} from "@/components/dash/payment/ServiceSelect";
 
 import {Service} from "@/types/ServiceType";
 
+import {service} from "@/utils/routes";
+
 export const ServiceItem = ({id, name}: Service) => {
   return (
     <li className={"w-full pt-5 flex flex-row justify-between items-center border-t border-gray-500"}>
-      <div className={"flex flex-row gap-2"}>
-        {/*<Image src={`${image}`} alt={"Logo del servicio"} width={30} height={30}/>*/}
+      <div className={"flex flex-row gap-5 h-8 items-center"}>
+        <Image className={"min-h-full max-h-full object-contain"} src={service[id].src} alt={service[id].alt} width={80} height={25}/>
         <span>{name}</span>
       </div>
       <ServiceSelect serviceId={id}/>
