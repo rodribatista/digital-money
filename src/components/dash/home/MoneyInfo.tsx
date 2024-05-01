83e30250bb38a5ae80b47414a813ff72cb621a08
Rodrigo <rodrigo@192.168.1.20>
2024-05-01 15:38:27 -0300

    [dev] change href from cvu link

--- src/components/dash/home/MoneyInfo.tsx
+++ src/components/dash/home/MoneyInfo.tsx
@@ -10,7 +10,7 @@ export const MoneyInfo = () => {
     <section className={"w-full p-5 flex flex-col gap-5 rounded-md bg-black shadow-md md:p-10 xl:p-15"}>
       <div className={"flex flex-row gap-5 self-end"}>
         <Link href={"/dashboard/cards"} className={"border-b text-sm"}>Ver tarjetas</Link>
-        <Link href={"/dashboard/profile"} className={"border-b text-sm"}>Ver CVU</Link>
+        <Link href={"/dashboard/deposit/bank"} className={"border-b text-sm"}>Ver CVU</Link>
       </div>
       <div className={"flex flex-col gap-5"}>
         <h2 className={"text-xl"}>Dinero disponible</h2>
