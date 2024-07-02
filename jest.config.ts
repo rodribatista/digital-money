a0263ec8e1924ed9b0948aba0c940a54aad8b2c0
Rodrigo <rodrigo@192.168.1.13>
2024-07-02 12:59:05 -0300

    [test] testing initial config

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+ 
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+}
+
+export default createJestConfig(config)
